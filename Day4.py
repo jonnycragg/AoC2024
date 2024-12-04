cca7f8626abf30e1bbe9120fde3c385eaded4f26
jonnycragg <jonnycragg@hotmail.com>
2024-12-04 16:31:53 +0000

    Update Day4.py
    different approach to get part 2 done - find an A and then look around up, down, left right and see what we have

--- Day4.py
+++ Day4.py
@@ -18,7 +18,42 @@ def checkforxmas(line):
     # count how many times xmas in this line and return that count
     reg1 = 'XMAS'
     matches = re.compile('(%s)' % (reg1)).findall(line)
-    return len(matches)
+    pattern = re.compile(reg1)
+    matches1 = pattern.finditer(line)
+    positions = [(match.start(), match.end()) for match in matches1]
+    return len(matches), positions
+
+def checkforXmasDummy(lines):
+    # search for A but skip first row, last row, first column last col
+    # if found A check around to see if MAS spelled diagonally around me in X format and if so count
+    # input looks like this: ['123456',
+    # input looks like this:  '123456',
+    # input looks like this:  '123456',
+    # input looks like this:  '123456',
+    # input looks like this:  '123456']
+    count = 0
+    # count how many times xmas in this line and return that count
+    total_num_lines = len(lines)
+    lcounter = -1
+    ccounter = -1
+    for x in lines:
+        lcounter = lcounter + 1
+        # ignore first and lat lines
+        if lcounter == 0: continue
+        if lcounter == len(lines)-1: continue
+        ccounter = -1
+        for y in x:
+            ccounter = ccounter + 1
+            # ignore first and last columns
+            if ccounter == 0: continue
+            if ccounter == len(x)-1: continue
+            if y == 'A':
+                wordl = lines[lcounter-1][ccounter-1] + y + lines[lcounter+1][ccounter+1]
+                wordr = lines[lcounter+1][ccounter-1] + y + lines[lcounter-1][ccounter+1]
+                if ''.join(sorted(wordl)) == ''.join(sorted(wordr)) == 'AMS':
+                    count = count + 1
+    print("XXMAS Dummy count of found " + str(count))
+    return count
 
 def getmatrix(test):
     max_col = len(test[0])
@@ -47,17 +82,26 @@ def getmatrix(test):
         all_lines.append(''.join(x))
     all_lines_rev = reverse_xmas(all_lines)
     xmas_count = 0
+    xmas_count_match_coords = []
     for lines in all_lines:
-        xmas_count = xmas_count + checkforxmas(lines)
+        count, cords = checkforxmas(lines)
+        xmas_count = xmas_count + count
+        xmas_count_match_coords.append(cords)
     for lines in all_lines_rev:
-        xmas_count = xmas_count + checkforxmas(lines)
+        count, cords = checkforxmas(lines)
+        xmas_count = xmas_count + count
+        xmas_count_match_coords.append(cords)
     print("xmas count = " + str(xmas_count))
+    print("xmas count coords = " + str(xmas_count_match_coords))
     return all_lines
 
 if (__name__ == "__main__"):
     print("Part Test Start")
     myInput1 = readData('D4TData.txt')
     getmatrix(myInput1)
-    print("Part 1 Real Data")
+    print("Part 2 Test Data")
+    myInput1 = readData('D4TData.txt')
+    checkforXmasDummy(myInput1)
+    print("Part 2 Real Data")
     myInput1 = readData('D4Data.txt')
-    getmatrix(myInput1)
+    checkforXmasDummy(myInput1)
