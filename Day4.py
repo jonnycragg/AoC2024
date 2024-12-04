a5ff43e77a3441cb3a75916701c17233048e6d0c
jonnycragg <jonnycragg@hotmail.com>
2024-12-04 10:16:24 +0000

    day4 - saving for now

--- Day4.py
+++ Day4.py
@@ -36,28 +36,47 @@ def checkforxmas(line):
     matches = re.compile('(%s)' % (reg1)).findall(line)
     return len(matches)
 
+def getmatrix(test):
+    #test = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['10', '11', '12']]
+    #test = ['MMMSXXMASM', 'MSAMXMSMSA', 'AMXSXMAAMM', 'MSAMASMSMX', 'XMASAMXAMM', 'XXAMMXXAMA', 'SMSMSASXSS', 'SAXAMASAAA', 'MAMMMXMMMM', 'MXMXAXMASX']
+    max_col = len(test[0])
+    max_row = len(test)
+    cols = [[] for _ in range(max_col)]
+    rows = [[] for _ in range(max_row)]
+    fdiag = [[] for _ in range(max_row + max_col - 1)]
+    bdiag = [[] for _ in range(len(fdiag))]
+    min_bdiag = -max_row + 1
+
+    for x in range(max_col):
+        for y in range(max_row):
+            cols[x].append(test[y][x])
+            rows[y].append(test[y][x])
+            fdiag[x + y].append(test[y][x])
+            bdiag[x - y - min_bdiag].append(test[y][x])
+
+    all_lines = []
+    all_lines_rev = []
+    for x in cols:
+        all_lines.append(''.join(x))
+    for x in rows:
+        all_lines.append(''.join(x))
+    for x in fdiag:
+        all_lines.append(''.join(x))
+    for x in bdiag:
+        all_lines.append(''.join(x))
+    all_lines_rev = reverse_xmas(all_lines)
+    xmas_count = 0
+    for lines in all_lines:
+        xmas_count = xmas_count + checkforxmas(lines)
+    for lines in all_lines_rev:
+        xmas_count = xmas_count + checkforxmas(lines)
+    print("xmas count = " + str(xmas_count))
+    return all_lines
+
 if (__name__ == "__main__"):
     print("Part Test Start")
     myInput1 = readData('D4TData.txt')
-    print("Part Test Input Rows = " + str(myInput1))
-    columns = up_down_xmas(myInput1)
-    print("Part Test Input Columns = " + str(columns))
-    rows_rev = reverse_xmas(myInput1)
-    columns_rev = reverse_xmas(columns)
-    print("Part Test Input Columns Reversed = " + str(columns_rev))
-    print("Part Test Input Rows reversed = " + str(rows_rev))
-    xmas_count = 0
-    for lines in myInput1:
-        xmas_count = xmas_count + checkforxmas(lines)
-    print("Part Test xmas Count Rows = " + str(xmas_count))
-    for lines in columns:
-        xmas_count = xmas_count + checkforxmas(lines)
-    print("Part Test xmas Count Cols = " + str(xmas_count))
-    for lines in rows_rev:
-        xmas_count = xmas_count + checkforxmas(lines)
-    print("Part Test xmas Count Rows reversed = " + str(xmas_count))
-    for lines in columns_rev:
-        xmas_count = xmas_count + checkforxmas(lines)
-    print("Part Test xmas Count Cols reversed = " + str(xmas_count))
-
+    getmatrix(myInput1)
     print("Part 1 Real Data")
+    myInput1 = readData('D4Data.txt')
+    getmatrix(myInput1)
